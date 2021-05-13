c481c6192b0573a2b68edd6d8dddd9788f83bed1
Katri <suntribe2020@gmail.com>
2021-05-13 18:44:30 +0200

    Added MyMathCalculator class and some methods

---
+++ src/main/java/com/example/springdocker/MyMathCalculator.java
@@ -0,0 +1,23 @@
+package com.example.springdocker;
+
+/**
+ * Created by Katri Vidén
+ * Date: 2021-05-13
+ * Time: 18:38
+ * Project: spring-docker-demo
+ * Copyright: MIT
+ */
+public class MyMathCalculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
