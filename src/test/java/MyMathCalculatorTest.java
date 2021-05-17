5a25f4394856a0feb1cfbffd807be4ca4616b3c6
Katri <suntribe2020@gmail.com>
2021-05-17 10:07:54 +0200

    Added tests

---
+++ src/test/java/MyMathCalculatorTest.java
@@ -0,0 +1,46 @@
+import com.example.springdocker.MyMathCalculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Created by Katri Vidén
+ * Date: 2021-05-14
+ * Time: 09:25
+ * Project: spring-docker-demo
+ * Copyright: MIT
+ */
+class MyMathCalculatorTest {
+
+    MyMathCalculator myMathCalculator;
+
+    @BeforeEach
+    public void beforeMethod() {
+        myMathCalculator = new MyMathCalculator();
+    }
+
+    @Test
+    void add() {
+        assertEquals(5, myMathCalculator.add(2, 3));
+    }
+
+    @Test
+    void multiply() {
+        assertEquals(25, myMathCalculator.multiply(5, 5));
+    }
+
+    @Test
+    void divide() {
+        assertEquals(10, myMathCalculator.divide(50,5));
+        assertEquals(8.2, myMathCalculator.divide(20.5,2.5));
+    }
+
+    @Test
+    void divideThrowsException() {
+        double input = 1.5;
+        double input2 = 0;
+        assertThrows(NullPointerException.class, () -> myMathCalculator.divide(input, input2));
+    }
+}
